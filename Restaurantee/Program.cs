1f89bad72ff5d5bc9e3001fc0f8a91f11c06145d
Gabriela Duque <ivan.navarro.vargas@gmail.com>
2020-02-25 10:07:51 +0100

    first commit

---
+++ Restaurantee/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fase 1
+
+            float cantidad;
+            int can, B500, B200, B100, B50, B20, B10, B5, M1;
+            B500 = 0;
+            B200 = 0;
+            B100 = 0;
+            B50 = 0;
+            B20 = 0;
+            B10 = 0;
+            B5 = 0;
+            M1 = 0;
+            can = 0;
+
+            string[] menu = new string[5];
+            int[] preciototal = new int[5];
+            int masComida;
+            bool comidaExiste = false;
+            bool keepdoing = true;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<String> lista  = new List<string>();
+
+            // FASE 2
+
+            for (int i = 0; i < menu.Length; i++)
+            {
+
+                Console.Write("Introduce un plato:");
+                menu[i] = Console.ReadLine();
+
+                Console.Write("Introduce el precio:");
+                preciototal[i] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            Console.WriteLine("el menu es el siguiente");
+
+            for (int i = 0; i < menu.Length; i++)
+            {
+               
+                Console.Write(menu[i] + " " + preciototal[i] + "€;" + "\n");
+            }
+            
+
+
+            while (keepdoing)
+            {
+                Console.WriteLine("Escribe lo que quieres comer");
+                lista.Add(Console.ReadLine());
+                Console.Write("Quieres pedir mÃ¡s comida? (1:Si / 0:No): ");
+                masComida = int.Parse(Console.ReadLine());
+
+                if (masComida == 0)
+                {
+                    keepdoing = false;
+                }
+            }
+
+            //FASE 3
+
+            foreach (var item in lista)
+            {
+                for (int i = 0; i < menu.Length; i++) 
+                {
+                    if (item == menu[i]) 
+                    {
+                        can += preciototal[i];
+                        comidaExiste = true;
+                        
+
+                    }
+                    
+
+                }
+
+
+                if (!comidaExiste)
+                {
+                    Console.WriteLine("No tenemos este plato: " + item);
+                }
+                comidaExiste = false;
+
+
+            }
+            // FASE4
+            string myString = can.ToString();
+            cantidad = float.Parse(myString);
+            Console.WriteLine("El precio total de tu comida es de: " + cantidad);
+
+            if ((cantidad >= 100))
+            {
+                B100 = ((int)cantidad / 100);
+                cantidad = cantidad - (B100 * 100);
+            }
+            if ((cantidad >= 50))
+            {
+                B50 = ((int)cantidad / 50);
+                cantidad = cantidad - (B50 * 50);
+            }
+            if ((cantidad >= 20))
+            {
+                B20 = ((int)cantidad / 20);
+                cantidad = cantidad - (B20 * 20);
+            }
+            if ((B10 >= 10))
+            {
+                B10 = ((int)cantidad / 10);
+                cantidad = cantidad - (B10 * 10);
+            }
+            if ((cantidad >= 5))
+            {
+                B5 = ((int)cantidad / 5);
+                cantidad = cantidad - (B5 * 5);
+            }
+            if ((cantidad >= 1))
+            {
+                M1 = ((int)cantidad / 1);
+                cantidad = cantidad - (M1 * 1);
+            }
+
+            Console.WriteLine("Billetes de 100: " + B100);
+            Console.WriteLine("Billetes de 50 : " + B50);
+            Console.WriteLine("Billetes de 20 : " + B20);
+            Console.WriteLine("Billetes de 10 : " + B10);
+            Console.WriteLine("Billetes de 5 : " + B5);
+            Console.WriteLine("Monedas de 1 : " + M1);
+
+
+            Console.Read();
+
+        }
+    }
+    
+}
---
+++ Restaurantee/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Restaurantee")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ajuntament de Barcelona")]
+[assembly: AssemblyProduct("Restaurantee")]
+[assembly: AssemblyCopyright("Copyright © Ajuntament de Barcelona 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8baab10d-55d6-4164-9d4b-3f83d08a2668")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
